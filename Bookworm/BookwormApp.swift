88cf15e82e342e457eac1c20f3112d40a664b370
Dragos Neacsu <dragosncs@gmail.com>
2023-08-21 11:44:42 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Dragos Neacsu on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
